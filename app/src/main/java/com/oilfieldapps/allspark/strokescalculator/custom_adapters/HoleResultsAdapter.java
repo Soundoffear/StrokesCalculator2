bd996ddb1efd98d62d6104cf16cc01bdf83200d4
soundoffear <m.dyngosz@gmail.com>
2017-12-25 02:44:48 +0400

    All result views changed from ListView to RecyclerView

---
+++ app/src/main/java/com/oilfieldapps/allspark/strokescalculator/custom_adapters/HoleResultsAdapter.java
@@ -0,0 +1,66 @@
+package com.oilfieldapps.allspark.strokescalculator.custom_adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.oilfieldapps.allspark.strokescalculator.R;
+import com.oilfieldapps.allspark.strokescalculator.data_and_databases.HoleResultsData;
+
+import java.util.List;
+
+public class HoleResultsAdapter extends RecyclerView.Adapter<HoleResultsAdapter.HoleResultsViewHolder> {
+
+    private Context hContext;
+    private List<HoleResultsData> holeResultsData;
+
+    public HoleResultsAdapter(Context hContext, List<HoleResultsData> holeResultsData) {
+        this.hContext = hContext;
+        this.holeResultsData = holeResultsData;
+    }
+
+    @Override
+    public HoleResultsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        int layoutR = R.layout.s_and_v_data_out_empty_hole_list_view;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(layoutR, parent, false);
+
+        return new HoleResultsViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(HoleResultsViewHolder holder, int position) {
+
+        holder.tv_hName.setText(holeResultsData.get(position).getName_hole_results());
+        holder.tv_hVolumeResult.setText(holeResultsData.get(position).getVolume_hole_results());
+        holder.tv_hVolumeUnits.setText(holeResultsData.get(position).getVolume_hole_results_units());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return holeResultsData.size();
+    }
+
+    class HoleResultsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_hName;
+        TextView tv_hVolumeResult;
+        TextView tv_hVolumeUnits;
+
+        public HoleResultsViewHolder(View itemView) {
+            super(itemView);
+
+            tv_hName = itemView.findViewById(R.id.list_view_hole_results_no_pipe_name);
+            tv_hVolumeResult = itemView.findViewById(R.id.hole_result_no_pipe_volume);
+            tv_hVolumeUnits = itemView.findViewById(R.id.hole_result_no_pipe_volume_units);
+        }
+
+    }
+}
--- app/src/main/java/com/oilfieldapps/allspark/strokescalculator/data_out/SNV_Results_EmptyHole.java
+++ app/src/main/java/com/oilfieldapps/allspark/strokescalculator/data_out/SNV_Results_EmptyHole.java
@@ -1,49 +1,38 @@
 package com.oilfieldapps.allspark.strokescalculator.data_out;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.oilfieldapps.allspark.strokescalculator.R;
-import com.oilfieldapps.allspark.strokescalculator.converters.Converter;
-import com.oilfieldapps.allspark.strokescalculator.custom_adapters.Hole_results_no_pipe_adapter;
-import com.oilfieldapps.allspark.strokescalculator.data_and_databases.HoleData;
-import com.oilfieldapps.allspark.strokescalculator.data_and_databases.HoleData_DataBase;
+import com.oilfieldapps.allspark.strokescalculator.custom_adapters.HoleResultsAdapter;
 import com.oilfieldapps.allspark.strokescalculator.data_and_databases.HoleResultsData;
 import com.oilfieldapps.allspark.strokescalculator.data_and_databases.HoleResults_DataBase;
 
-import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Created by Allspark on 07/09/2017.
- */
 
 public class SNV_Results_EmptyHole extends Fragment {
 
-    ListView holeDataResultsListView;
-
-    Hole_results_no_pipe_adapter hole_results_no_pipe_adapter;
+    RecyclerView holeDataResultsRecView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.s_and_v_data_out_empty_hole, null);
 
-        holeDataResultsListView = view.findViewById(R.id.listView_results_hole);
-
+        holeDataResultsRecView = view.findViewById(R.id.recView_results_hole);
         HoleResults_DataBase holeResults_dataBase = new HoleResults_DataBase(getContext());
-
         List<HoleResultsData> holeResultsDatas = holeResults_dataBase.getAllResults();
 
-        hole_results_no_pipe_adapter = new Hole_results_no_pipe_adapter(getContext(), holeResultsDatas);
-        holeDataResultsListView.setAdapter(hole_results_no_pipe_adapter);
+        holeDataResultsRecView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        holeDataResultsRecView.setLayoutManager(linearLayoutManager);
+        HoleResultsAdapter holeResultsAdapter = new HoleResultsAdapter(getContext(), holeResultsDatas);
+        holeDataResultsRecView.setAdapter(holeResultsAdapter);
 
         return view;
     }
